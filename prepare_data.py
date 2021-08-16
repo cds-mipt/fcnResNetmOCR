cc27bba91331dc50a271eb9397774f979663ae96
Vasiliy Adeshkin <adeshkin.vi@phystech.edu>
2021-08-16 11:19:02 +0300

    Update prepare_data.py
    add ratio

--- prepare_data.py
+++ prepare_data.py
@@ -4,7 +4,7 @@ import random
 from tqdm import tqdm
 
 
-def crop_resize(data_dir, save_dir, new_size=(1920, 1080)):
+def crop_resize_v1(data_dir, save_dir, new_size=(1920, 1080)):
     image_dir = os.path.join(data_dir, "images")
     label_dir = os.path.join(data_dir, "labels")
     
@@ -37,7 +37,49 @@ def crop_resize(data_dir, save_dir, new_size=(1920, 1080)):
 
         new_image.save(new_image_path)
         new_label.save(new_label_path)
+        
+        
+def crop_resize(data_dir, save_dir, new_size=(1920, 1080)):
+    image_dir = os.path.join(data_dir, "images")
+    label_dir = os.path.join(data_dir, "labels")
 
+    new_image_dir = os.path.join(save_dir, "images")
+    os.makedirs(new_image_dir)
+    new_label_dir = os.path.join(save_dir, "labels")
+    os.makedirs(new_label_dir)
+
+    image_names = sorted(os.listdir(image_dir))
+    for image_name in tqdm(image_names):
+        image_path = os.path.join(image_dir, image_name)
+        label_path = os.path.join(label_dir, image_name[:-3] + "png")
+
+        image = Image.open(image_path)
+        label = Image.open(label_path)
+        w, h = image.size
+        w_target, h_target = new_size
+        ratio = w_target / h_target
+        new_h = round(w / ratio)
+
+        if new_h <= h:
+            h_shift = random.randint((h - new_h) // 2, h - new_h)
+
+            image = image.crop((0, h_shift, w, h_shift + new_h))
+            label = label.crop((0, h_shift, w, h_shift + new_h))
+        else:
+            new_w = round(h * ratio)
+            w_shift = random.randint((w - new_w) // 2, w - new_w)
+
+            image = image.crop((w_shift, 0, w_shift + new_w, h))
+            label = label.crop((w_shift, 0, w_shift + new_w, h))
+
+        new_image = image.resize(new_size)
+        new_label = label.resize(new_size)
+
+        new_image_path = os.path.join(new_image_dir, image_name)
+        new_label_path = os.path.join(new_label_dir, image_name[:-3] + "png")
+
+        new_image.save(new_image_path)
+        new_label.save(new_label_path)
 
 def main():
     data_root = "/datasets/Mapillary/mapillary-vistas-dataset_public_v1.1"
